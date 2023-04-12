1bfb32030297ad7fb49d49ac99674f8ce0014029
drori200 <droript05@gmail.com>
2023-04-12 23:42:47 +0300

    Update LateMixinLoader.java

--- src/main/java/com/goatee/tutorial/LateMixinLoader.java
+++ src/main/java/com/goatee/tutorial/LateMixinLoader.java
@@ -2,11 +2,11 @@ package com.goatee.tutorial;
 
 import com.gtnewhorizon.gtnhmixins.ILateMixinLoader;
 import com.gtnewhorizon.gtnhmixins.LateMixin;
+import org.spongepowered.asm.mixin.MixinEnvironment;
+import org.spongepowered.asm.mixin.MixinEnvironment.Side;
+import org.spongepowered.asm.service.MixinService;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @LateMixin
 public class LateMixinLoader implements ILateMixinLoader {
@@ -24,8 +24,15 @@ public class LateMixinLoader implements ILateMixinLoader {
      */
     @Override
     public List<String> getMixins(Set<String> loadedMods) {
-        List<String> toLoad = new ArrayList<String>();
-        String[] mixins = {"MixinIDBCPlayer", "MixinIPlayer", "MixinJRMCoreH", "MixinScriptDBCPlayer", "MixinScriptPlayer"};
-        return Arrays.asList(mixins);
+        List<String> toLoad = new ArrayList<>();
+        String[] CommonMixins = {"MixinIDBCPlayer", "MixinIPlayer", "MixinJRMCoreH", "MixinScriptDBCPlayer"};
+        String[] ClientMixins = {"MixinScriptPlayer"};
+        String[] ServerMixins = {};
+        Collections.addAll(toLoad, CommonMixins);
+        if(MixinService.getService().getSideName().equals(Side.CLIENT.name()))
+            Collections.addAll(toLoad, ClientMixins);
+        if(MixinService.getService().getSideName().equals(Side.SERVER.name()))
+            Collections.addAll(toLoad, ServerMixins);
+        return toLoad;
     }
 }
